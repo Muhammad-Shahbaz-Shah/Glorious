f9b25485173e9b7de38c5479a409b0b14f937567
Syed Shahbaz <syedshahbazshah7570@gmail.com>
2026-01-10 18:38:29 +0500

    finnal better auth fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -30,6 +30,7 @@ const getVerificationEmailHtml = (url) => `
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
   emailAndPassword: {
     enabled: true,
     forgetPassword: {
